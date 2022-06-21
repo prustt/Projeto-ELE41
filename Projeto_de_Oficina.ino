8d43786f61320a5bf9a2c7ef039f4043bf2266ba
Luiz Alberto Prust <luizprust@hotmail.com>
2022-06-20 21:00:45 -0300

    .ino file commit

---
+++ Projeto_de_Oficina.ino
@@ -0,0 +1,140 @@
+#include "LedControl.h"
+
+/*Now we need a LedControl to work with.
+pin 4 is connected to the DataIn
+pin 5 is connected to the CLK
+pin 6 is connected to LOAD / CS
+We only have a single MAX7219 */
+
+LedControl lc=LedControl(4,6,5,1);
+float vrtotal;
+int linha;
+int coluna;
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=500;
+void setup()
+{
+/* The MAX72XX is in power-saving mode on startup,
+we have to do a wakeup call */
+lc.shutdown(0,false); //* Set the brightness to a medium values */
+lc.setIntensity(0,4); //* and clear the display */
+lc.clearDisplay(0);
+
+Serial.begin(57600);
+
+}
+
+void loop()
+{
+  float VR = analogRead(A1);
+ 
+  vrtotal = 1000*((3.8*VR)/1023);
+
+  Serial.print(VR);
+  Serial.print("   ");
+  Serial.print(vrtotal);
+  Serial.println("mV");
+
+  if(vrtotal>=1500){
+      for(int it=0;it<=7;it++){
+        for(int j=0; j<=7; j++){
+          lc.setLed(0, it, j, true);
+        }
+    }
+  }
+  if(vrtotal>=100){
+      lc.setLed(0, 0, 0, true);
+  }
+  if(vrtotal>=200){
+    for(int it=0;it<=1;it++){
+      lc.setLed(0, 1, it, true);
+    }
+  }
+  if(vrtotal>=300){
+    for(int it=0;it<=2;it++){
+      lc.setLed(0, 2, it, true);
+    }
+  }
+  if(vrtotal>=400){
+    for(int it=0;it<=3;it++){
+      lc.setLed(0, 3, it, true);
+    }
+  }
+  if(vrtotal>=500){
+    for(int it=0;it<=4;it++){
+      lc.setLed(0, 4, it, true);
+    }
+  }
+  if(vrtotal>=600){
+    for(int it=0;it<=5;it++){
+      lc.setLed(0, 5, it, true);
+    }
+  }
+  if(vrtotal>=700){
+    for(int it=0;it<=6;it++){
+      lc.setLed(0, 6, it, true);
+    }
+  }
+  if(vrtotal>=800){
+    for(int it=0;it<=7;it++){
+      lc.setLed(0, 7, it, true);
+    }
+  }
+  if(vrtotal>=900){
+      linha=6;
+      coluna=0;      
+    for(int it=0;it<=7;it++){
+      lc.setLed(0, linha, it-coluna, true);
+      if(linha>=0)  linha--;
+      if(coluna<=6) coluna++;
+    }
+  }
+  if(vrtotal>=1000){
+    linha=5;
+    coluna=0;
+    for(int it=0;it<=7;it++){
+      lc.setLed(0, linha, it-coluna, true);
+      if(linha>=0)  linha--;
+      if(coluna<=5) coluna++;
+    }
+  }
+  if(vrtotal>=1100){
+    linha=4;
+    coluna=0;
+    for(int it=0;it<=7;it++){
+      lc.setLed(0, linha, it-coluna, true);
+      if(linha>=0)  linha--;
+      if(coluna<=4) coluna++;
+    }
+  }
+  if(vrtotal>=1200){
+    linha=3;
+    coluna=0;
+    for(int it=0;it<=7;it++){
+      lc.setLed(0, linha, it-coluna, true);
+      if(linha>=0)  linha--;
+      if(coluna<=3) coluna++;
+      
+    }
+  }
+  if(vrtotal>=1300){
+    linha=2;
+    coluna=0;
+    for(int it=0;it<=7;it++){
+      lc.setLed(0, linha, it-coluna, true);
+      if(linha>=0)  linha--;
+      if(coluna<=2) coluna++;
+    }
+  }
+  if(vrtotal>=1400){
+    for(int it=0;it<=7;it++){
+      lc.setLed(0, 1, it, true);
+      lc.setLed(0, 0, it-1, true);
+    }
+  }
+ 
+  if(vrtotal==0){
+    lc.clearDisplay(0);
+  }
+  delay(25);
+}
